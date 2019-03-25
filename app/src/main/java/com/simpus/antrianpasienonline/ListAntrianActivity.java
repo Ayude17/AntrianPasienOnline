fe840cf49d4b9fe93219a816f39362b11428ea3b
alesana001 <malela001@gmail.com>
2019-03-25 12:50:20 +0700

    revisi tanggal 25-03-2019

---
+++ app/src/main/java/com/simpus/antrianpasienonline/ListAntrianActivity.java
@@ -0,0 +1,5 @@
+package com.simpus.antrianpasienonline;
+
+public class ListAntrianActivity {
+
+}
--- app/src/main/java/com/simpus/antrianpasienonline/PilihDokterActivity.java
+++ app/src/main/java/com/simpus/antrianpasienonline/PilihDokterActivity.java
@@ -7,16 +7,16 @@ import android.view.View;
 import android.widget.Button;
 
 public class PilihDokterActivity extends AppCompatActivity {
-    Button btn_kembali3;
+    Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilih_dokter);
 
-        btn_kembali3 =(Button)findViewById(R.id.btn_next );
+        btn_next =(Button)findViewById(R.id.btn_next );
 
-        btn_kembali3.setOnClickListener(new View.OnClickListener() {
+        btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent Intentantrian = new Intent(PilihDokterActivity.this, KartuAntrianActivity.class);

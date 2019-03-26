1d4bb56612dd60a210182d282dab43491418f6e2
alesana001 <malela001@gmail.com>
2019-03-26 16:00:44 +0700

    revisi tanggal 25-03-2019

--- app/src/main/java/com/simpus/antrianpasienonline/PilihTanggalActivity.java
+++ app/src/main/java/com/simpus/antrianpasienonline/PilihTanggalActivity.java
@@ -1,17 +1,51 @@
 package com.simpus.antrianpasienonline;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class PilihTanggalActivity extends AppCompatActivity {
     Button btn_lanjut1;
+    CalendarView calendarView;
+    TextView tanggalView;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilih_tanggal);
+
+        //inisiasi calendar
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        tanggalView = (TextView) findViewById(R.id.tanggalView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange( @NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String formattedMonth = "" + month;
+                String formattedDayOfMonth = "" + dayOfMonth;
+                if(month < 10){
+
+                    formattedMonth = "0" + month;
+                }
+                if(dayOfMonth < 10){
+
+                    formattedDayOfMonth = "0" + dayOfMonth;
+                }
+
+                tanggalView.setText(year + "-" + formattedMonth + "-" +formattedDayOfMonth );
+            }
+        });
 
         //menginisiasikan button
         btn_lanjut1= (Button) findViewById(R.id.btn_lanjut1);

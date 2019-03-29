bf6bf8ec83ec8f8544921c07eb407596748c3cef
Simpus Desktop <ayude17@gmail.com>
2019-03-29 10:30:35 +0700

    com29Mar19

--- app/src/main/java/com/simpus/antrianpasienonline/MenuActivity.java
+++ app/src/main/java/com/simpus/antrianpasienonline/MenuActivity.java
@@ -2,6 +2,7 @@ package com.simpus.antrianpasienonline;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button dafAntrian, antrianSaya;
+    private Button dafAntrian, antrianSaya, btnlogout;
     TextView txt_username;
     String username;
 
@@ -21,11 +22,13 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         //mendefinisikan variable
         dafAntrian =(Button)findViewById(R.id.dafAntrian );
         antrianSaya =(Button)findViewById(R.id.antrianSaya );
+        btnlogout = (Button) findViewById(R.id.btn_logout);
         txt_username = (TextView)findViewById(R.id.txt_username);
 
         //mendefinisikan click listener
         dafAntrian.setOnClickListener(this);
         antrianSaya.setOnClickListener(this);
+        btnlogout.setOnClickListener(this);
         username = getSharedPreferences("DATA", Context.MODE_PRIVATE).getString("nama","");
 
                 //mengubah tulisan username
@@ -48,7 +51,21 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this, "anda memilih antrian saya", Toast.LENGTH_SHORT).show();
                 break;
 
+            case R.id.btn_logout:
+                SharedPreferences sharedPreferences = getSharedPreferences("DATA", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("login", false);
+//                editor.putString(TAG_USERNAME,null);
+                editor.commit();
+
+                Intent intent = new Intent(MenuActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+
+//                return true;
         }
+//        return super.onOptionsItemSelected(v);
     }
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
